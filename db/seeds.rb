ee545fce4510b9aeba5b5274ac4f434745397026
Natalia Passos <nathypassos@gmail.com>
2023-02-25 15:46:20 -0300

    created seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Bet.destroy_all
+
+Bet.create(
+  description: "vai chover até as 15h",
+  amount: 10,
+  start_date: DateTime.yesterday,
+  end_date: DateTime.tomorrow,
+  status: "open"
+)
+
+Bet.create(
+  description: "Victor vai beber até as 18:30",
+  amount: 10,
+  start_date: DateTime.yesterday,
+  end_date: DateTime.tomorrow,
+  status: "open"
+)
+
+Bet.create(
+  description: "Bruno vai pegar o buquê",
+  amount: 10,
+  start_date: DateTime.yesterday,
+  end_date: DateTime.tomorrow,
+  status: "open"
+)
