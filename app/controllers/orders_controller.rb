afa6bd33fd2c2f0ae198e12b1f461f38509d47c4
Bruno Carceles <bruno0092@gmail.com>
2023-03-09 20:26:29 -0300

    funcionou somar no stripe

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -29,6 +29,33 @@
 #   end
 # end
 
+# class OrdersController < ApplicationController
+#   def create
+#     chip = Chip.find(params[:chip_id])
+#     @order = Order.create!(chip: chip, chip_name: chip.name, amount: chip.price, state: 'pending', user: current_user)
+#     authorize @order
+#     session = Stripe::Checkout::Session.create(
+#       payment_method_types: ['card'],
+#       line_items: [{
+#         price_data: {
+#           currency: 'brl',
+#           product_data: {
+#             name: chip.name
+#           },
+#           unit_amount: chip.price_cents
+#         },
+#         quantity: 1
+#       }],
+#       mode: 'payment',
+#       success_url: update_balance_url(user_id: current_user.id),
+#       cancel_url: user_wallet_url(user_id: current_user.id)
+#     )
+
+#     @order.update(checkout_session_id: session.id)
+#     redirect_to new_order_payment_path(@order)
+#   end
+# end
+
 class OrdersController < ApplicationController
   def create
     chip = Chip.find(params[:chip_id])
@@ -47,11 +74,16 @@ class OrdersController < ApplicationController
         quantity: 1
       }],
       mode: 'payment',
-      success_url: user_wallet_url(user_id: current_user.id, amount: chip.price_cents, controller: 'wallets', action: 'update_balance'),
+      success_url: user_wallet_url(user_id: current_user.id),
       cancel_url: user_wallet_url(user_id: current_user.id)
     )
 
     @order.update(checkout_session_id: session.id)
+    # current_user.wallet.balance += chip.price_cents
+    # current_user.save
+    @wallet = Wallet.find_by(user: current_user)
+    balance = @wallet.balance
+    @wallet.update(balance: (balance + (chip.price_cents / 100)))
     redirect_to new_order_payment_path(@order)
   end
 end
