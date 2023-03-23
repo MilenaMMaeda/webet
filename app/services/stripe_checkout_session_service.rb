c53a42dd4fc7ff6d5588d0360363a9eaf15d63cf
Milena Maeda <milenaminekawa@gmail.com>
2023-03-23 15:12:54 -0500

    fixed stripe class

--- app/services/stripe_checkout_session_service.rb
+++ app/services/stripe_checkout_session_service.rb
@@ -1,4 +1,4 @@
-# class StripeCheckoutSessionService
+class StripeCheckoutSessionService
 #   def call(event)
 #     order = Order.find_by(checkout_session_id: event.data.object.id)
 #     order.update(state: 'paid')
@@ -6,7 +6,7 @@
 #     # balance = @wallet.balance
 #     # @wallet.update(balance: (balance + (chip.price_cents / 100)))
 #   end
-# end
+end
 
 
 # descomentar o stripe.rb de initializar pra fazer a mudança aqui, mas só rola depois q estiver tudo configurado
